4e9b2f2d17026a6c85be456f717b02fef6c43814
UselessAmber <mainrurikoproject@gmail.com>
2022-10-15 18:01:10 +0700

    module 4 update (this is going to take a while...)

--- module4.py
+++ module4.py
@@ -53,4 +53,14 @@ print(format_address("1001 1st Ave"))
 print(format_address("55 North Center Drive"))
 # Should print "house number 55 on street named North Center Drive"
 
+# Problem 2
+# The highlight_word function changes the given word in a sentence to its upper-case version. 
+# For example, highlight_word("Have a nice day", "nice") returns "Have a NICE day". 
+# Can you write this function in just one line?
 
+def highlight_word(sentence, word):
+	return 
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
